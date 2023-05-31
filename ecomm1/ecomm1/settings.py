a31444572de78ccbcc9409a680416debf00d3422
Shivam <shivamsxn458@gmail.com>
2023-05-31 17:06:45 +0530

    Update settings.py

--- ecomm1/ecomm1/settings.py
+++ ecomm1/ecomm1/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/home/ubuntu/Djangoprojects/ecomm1/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR ,'media')
