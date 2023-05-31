aa4465a368f5ce09f53c8a861724dbe9fe731a47
Shivam <shivamsxn458@gmail.com>
2023-05-31 12:15:23 +0530

    Update settings.py

--- ecomm1/ecomm1/settings.py
+++ ecomm1/ecomm1/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
