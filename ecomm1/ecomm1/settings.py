1dd4d74bc9445612bbfd68f9f8a6671ba9b3b3ec
Shivam <shivamsxn458@gmail.com>
2023-06-12 23:44:42 +0530

    Update settings.py

--- ecomm1/ecomm1/settings.py
+++ ecomm1/ecomm1/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ssfhg1x@a^s9xdmrye3_(pvf=7b*a5ha!25h+v1k#is4!z^e9t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
