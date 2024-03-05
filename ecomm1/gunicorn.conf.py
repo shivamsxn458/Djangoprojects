feb70001d76459d175cb4b8bb406fb6600029dcc
Shivam <shivamsxn458@gmail.com>
2024-03-06 00:24:00 +0530

    Delete ecomm1 directory

--- ecomm1/gunicorn.conf.py
+++
@@ -1,6 +0,0 @@
-import multiprocessing
-
-#bind = "127.0.0.1:8000"  # Change the IP and port as needed
-workers = multiprocessing.cpu_count() * 2 
-worker_class = "gthread"  # For better concurrency with Django 3.0+
-timeout = 120
