11fbdd6aabb5be883994c0dd5f9f3c868a3635b2
Shivam <shivamsxn458@gmail.com>
2024-03-27 11:17:45 +0530

    Update app.py2

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
-# app.py
-print("Hello, This is a Hello world python application by Shivam Saxena!")
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b'Hello, World!')
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Starting server on port {port}...")
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
